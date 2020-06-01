05c79ec78c7ead6809d29db0bb36ac429ad8c1f3
Amiinoz <momagan@gmail.com>
2020-06-01 15:42:47 -0600

    Server is recieving data

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const cors = require('cors');
 
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 // define what happens whn client makes request
 app.get('/', (req, res) => {
